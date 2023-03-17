5d5b1cc65edf036b5f234ee5f1ea925188ae14b8
dammiik <damii@poczta.onet.pl>
2023-03-17 22:54:29 +0200

    fixed errors in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 mod common;
 mod vertex_data;
-
-fn create_vertices(r_torus: f32, r_tube: f32, n_torus: usize, n_tube:usize) -> Vec<common::Vertex> {
-    let(pos, normal) = vertex_data::torus_data(r_torus, r_tube, n_torus, n_tube);
-    let mut data:Vec<common::Vertex> = Vec::with_capacity(pos.len());
+fn create_vertices(
+    r_torus: f32,
+    r_tube: f32,
+    n_torus: usize,
+    n_tube: usize,
+) -> Vec<common::Vertex> {
+    let (pos, normal) = vertex_data::torus_data(r_torus, r_tube, n_torus, n_tube);
+    let mut data: Vec<common::Vertex> = Vec::with_capacity(pos.len());
+    let v: [f32; 3] = [0.0, 1.0, 0.0];
     for i in 0..pos.len() {
-        data.push(common::vertex(pos[i], normal[i]));
+        data.push(common::vertex(pos[i], normal[i], v));
     }
     data.to_vec()
 }
 
-fn main(){
+fn main() {
     let vertex_data = create_vertices(1.5, 0.4, 100, 50);
-    let light_data = common::light([1.0,0.0,0.0], [1.0, 1.0, 0.0], 0.1, 0.6, 0.3, 30.0);
-    common::run(&vertex_data, light_data, "torus");
+    let light_data = common::light([1.0, 0.0, 0.0], 0.1, 0.1, 0.6, 0.3, 0);
+    common::run(&vertex_data, light_data, "torus", "xxx");
 }
